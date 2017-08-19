326fd9497568f9994c601b23359de082f5da06e5
Mahdi <Mahdi@MAHDI-PC>
2017-08-19 21:53:11 +0430

    First Version

--- SamMobileLinkGenerator/Form1.Designer.cs
+++ SamMobileLinkGenerator/Form1.Designer.cs
@@ -28,12 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.thirteenForm1 = new Teen.ThirteenForm();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGen = new Teen.ThirteenTextBox();
+            this.txtDL = new Teen.ThirteenTextBox();
+            this.txtPS = new Teen.ThirteenTextBox();
+            this.txtUS = new Teen.ThirteenTextBox();
+            this.btnGit = new Teen.ThirteenButton();
+            this.btn = new Teen.ThirteenButton();
+            this.thirteenControlBox21 = new Teen.ThirteenControlBox2();
+            this.btnCopy = new Teen.ThirteenButton();
+            this.thirteenForm1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // thirteenForm1
+            // 
+            this.thirteenForm1.AccentColor = System.Drawing.Color.DodgerBlue;
+            this.thirteenForm1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenForm1.ColorScheme = Teen.ThirteenForm.ColorSchemes.Dark;
+            this.thirteenForm1.Controls.Add(this.label3);
+            this.thirteenForm1.Controls.Add(this.label2);
+            this.thirteenForm1.Controls.Add(this.label1);
+            this.thirteenForm1.Controls.Add(this.txtGen);
+            this.thirteenForm1.Controls.Add(this.txtDL);
+            this.thirteenForm1.Controls.Add(this.txtPS);
+            this.thirteenForm1.Controls.Add(this.txtUS);
+            this.thirteenForm1.Controls.Add(this.btnCopy);
+            this.thirteenForm1.Controls.Add(this.btnGit);
+            this.thirteenForm1.Controls.Add(this.btn);
+            this.thirteenForm1.Controls.Add(this.thirteenControlBox21);
+            this.thirteenForm1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.thirteenForm1.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.thirteenForm1.ForeColor = System.Drawing.Color.White;
+            this.thirteenForm1.Location = new System.Drawing.Point(0, 0);
+            this.thirteenForm1.Name = "thirteenForm1";
+            this.thirteenForm1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.thirteenForm1.Size = new System.Drawing.Size(622, 244);
+            this.thirteenForm1.TabIndex = 0;
+            this.thirteenForm1.Text = "SamMobile Link Generator       Coded By Mahdi72     ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Download Link";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "UserName";
+            // 
+            // txtGen
+            // 
+            this.txtGen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.txtGen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtGen.ColorScheme = Teen.ThirteenTextBox.ColorSchemes.Dark;
+            this.txtGen.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.txtGen.ForeColor = System.Drawing.Color.White;
+            this.txtGen.Location = new System.Drawing.Point(114, 207);
+            this.txtGen.Name = "txtGen";
+            this.txtGen.ReadOnly = true;
+            this.txtGen.Size = new System.Drawing.Size(470, 25);
+            this.txtGen.TabIndex = 2;
+            // 
+            // txtDL
+            // 
+            this.txtDL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.txtDL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDL.ColorScheme = Teen.ThirteenTextBox.ColorSchemes.Dark;
+            this.txtDL.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.txtDL.ForeColor = System.Drawing.Color.White;
+            this.txtDL.Location = new System.Drawing.Point(114, 125);
+            this.txtDL.Name = "txtDL";
+            this.txtDL.Size = new System.Drawing.Size(470, 25);
+            this.txtDL.TabIndex = 2;
+            // 
+            // txtPS
+            // 
+            this.txtPS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.txtPS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPS.ColorScheme = Teen.ThirteenTextBox.ColorSchemes.Dark;
+            this.txtPS.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.txtPS.ForeColor = System.Drawing.Color.White;
+            this.txtPS.Location = new System.Drawing.Point(114, 83);
+            this.txtPS.Name = "txtPS";
+            this.txtPS.Size = new System.Drawing.Size(470, 25);
+            this.txtPS.TabIndex = 2;
+            // 
+            // txtUS
+            // 
+            this.txtUS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.txtUS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUS.ColorScheme = Teen.ThirteenTextBox.ColorSchemes.Dark;
+            this.txtUS.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.txtUS.ForeColor = System.Drawing.Color.White;
+            this.txtUS.Location = new System.Drawing.Point(114, 52);
+            this.txtUS.Name = "txtUS";
+            this.txtUS.Size = new System.Drawing.Size(470, 25);
+            this.txtUS.TabIndex = 2;
+            // 
+            // btnGit
+            // 
+            this.btnGit.AccentColor = System.Drawing.Color.DodgerBlue;
+            this.btnGit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnGit.ColorScheme = Teen.ThirteenButton.ColorSchemes.Dark;
+            this.btnGit.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.btnGit.ForeColor = System.Drawing.Color.White;
+            this.btnGit.Location = new System.Drawing.Point(12, 168);
+            this.btnGit.Name = "btnGit";
+            this.btnGit.Size = new System.Drawing.Size(87, 23);
+            this.btnGit.TabIndex = 1;
+            this.btnGit.Text = "GitHub";
+            this.btnGit.UseVisualStyleBackColor = false;
+            this.btnGit.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn
+            // 
+            this.btn.AccentColor = System.Drawing.Color.DodgerBlue;
+            this.btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btn.ColorScheme = Teen.ThirteenButton.ColorSchemes.Dark;
+            this.btn.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.btn.ForeColor = System.Drawing.Color.White;
+            this.btn.Location = new System.Drawing.Point(141, 168);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(401, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Generate";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // thirteenControlBox21
+            // 
+            this.thirteenControlBox21.AccentColor = System.Drawing.Color.DodgerBlue;
+            this.thirteenControlBox21.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.thirteenControlBox21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.thirteenControlBox21.ColorScheme = Teen.ThirteenControlBox2.ColorSchemes.Dark;
+            this.thirteenControlBox21.ForeColor = System.Drawing.Color.White;
+            this.thirteenControlBox21.Location = new System.Drawing.Point(505, 3);
+            this.thirteenControlBox21.Name = "thirteenControlBox21";
+            this.thirteenControlBox21.Size = new System.Drawing.Size(100, 25);
+            this.thirteenControlBox21.TabIndex = 0;
+            this.thirteenControlBox21.Text = "thirteenControlBox21";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.AccentColor = System.Drawing.Color.DodgerBlue;
+            this.btnCopy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnCopy.ColorScheme = Teen.ThirteenButton.ColorSchemes.Dark;
+            this.btnCopy.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F);
+            this.btnCopy.ForeColor = System.Drawing.Color.White;
+            this.btnCopy.Location = new System.Drawing.Point(12, 206);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(87, 23);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = false;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 244);
+            this.Controls.Add(this.thirteenForm1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.thirteenForm1.ResumeLayout(false);
+            this.thirteenForm1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Teen.ThirteenForm thirteenForm1;
+        private Teen.ThirteenControlBox2 thirteenControlBox21;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Teen.ThirteenTextBox txtDL;
+        private Teen.ThirteenTextBox txtPS;
+        private Teen.ThirteenTextBox txtUS;
+        private Teen.ThirteenButton btn;
+        private Teen.ThirteenTextBox txtGen;
+        private Teen.ThirteenButton btnGit;
+        private Teen.ThirteenButton btnCopy;
     }
 }
 
--- SamMobileLinkGenerator/Form1.cs
+++ SamMobileLinkGenerator/Form1.cs
@@ -15,5 +15,33 @@ namespace SamMobileLinkGenerator
         {
             InitializeComponent();
         }
+        //http://dl.sammobile.com/M1FbQzklJDtRKjQxQjcuTUZYOSA5J0AsNChUKCA4O1oOARlJUyA5RSJXV0cjUlNFQ1BcRV9JTg4EAhYSSVJfREdfWEZe/G920PVPS4DQF1_G920PSPT4DQF1_SPR.zip
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (txtUS.Text == string.Empty || txtPS.Text==string.Empty)
+            {
+                MessageBox.Show("UserName or Password is Empty!");
+            }else if (txtDL.Text == string.Empty)
+            {
+                MessageBox.Show("Download Link is Empty!");
+            }
+            else
+            {
+                var _fixdl1 = txtDL.Text.Replace("https://", "");
+                var _fixdl2 = _fixdl1.Replace("http://", "");
+                txtGen.Text = "http://" + txtUS.Text + ":" + txtPS.Text + "@" + "www." + _fixdl2;
+            }
+           
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtGen.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            thirteenForm1.Text += "   " + ProductVersion;
+        }
     }
 }
